d2a797d5ffec6b2fb88f8b2e3b0ad3b35ed15840
btuerel <btuerel1@smail.uni-koeln.de>
2024-10-31 15:28:21 +0100

    aenderung

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,31 +4,6 @@
 Implement the logistic function and binary cross-entropy loss function, using titanic data.
 """
 
-import numpy as np
-import pandas as pd
-import seaborn as sns
-
-
-def logistic_function(x, a=0, b=0):
-    pass
-
-
-# binary cross-entropy loss
-def bcel(y, y_hat):
-    pass
-
-
-def read_data(path):
-    pass
-
-
-def plot(dataframe):
-    pass
-
-
-def loss(dataframe):
-    pass
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
