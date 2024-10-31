755d228e5e05fbd9436e3318090d7fc8c2337f6c
Janis Pagel <janis.pagel@uni-koeln.de>
2024-10-31 11:38:57 +0100

    Add exercise

---
+++ logistic_regression.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Implement the logistic function and binary cross-entropy loss function, using titanic data.
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+def logistic_function(x, a=0, b=0):
+    pass
+
+
+# binary cross-entropy loss
+def bcel(y, y_hat):
+    pass
+
+
+def read_data(path):
+    pass
+
+
+def plot(dataframe):
+    pass
+
+
+def loss(dataframe):
+    pass
