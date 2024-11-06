2464db3726ed28700216bf9ca5ff3c9ed1cf3bd0
FadeAlex <130512023+FadeAlex@users.noreply.github.com>
2024-11-06 19:14:07 +0000

    worked on it

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,21 +9,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#gibt y dach
 def logistic_function(x, a=0, b=0):
     lg = 1 / (1 + np.exp(-(a * x + b)))
     return lg
 
-
 # binary cross-entropy loss
 def bcel(y, y_hat):
-    bceloss = -(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+    bceloss = -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
     return bceloss
 
-
 def read_data(path):
-    dataframe = pd.read_csv(path)
+    dataframe = pd.read_csv(path, header = 0)
     return dataframe
-
 
 def plot(dataframe):
     plt.subplot(2,1,1)
@@ -38,9 +36,11 @@ def plot(dataframe):
     
     plt.show()
 
-
-def loss(dataframe):
-    pass
+def loss(dataframe, a, b):
+    y = dataframe["Survived"]
+    y_hat = logistic_function(dataframe["Age"], a, b)
+    loss = bcel(y, y_hat)
+    return loss
 
 file = read_data("titanic.csv")
 plot(file)
