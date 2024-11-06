699e9d4d46775661c6605149f5fce8aa10c3ba0c
Jago- <Jago-@192.168.2.163>
2024-11-06 20:25:44 +0100

    All done

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,29 +11,37 @@ import seaborn as sns
 
 
 def logistic_function(x, a=0, b=0):
-    database = pd.read_csv('titanic.csv', header = 0) 
-    #print(database)
-    sns.scatterplot(data = database, x = x, y = 'Survived')
-    plt.show()
+    y_hat = 1/(1 + np.exp(a*x+b))
+    return y_hat
     
-
-
-
 # binary cross-entropy loss
 def bcel(y, y_hat):
-    pass
+    return np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
 
 
 def read_data(path):
-    pass
+     dataframe = pd.read_csv(path, header = 0) 
+     #print(database)
+     return dataframe
 
 
-def plot(dataframe):
-    pass
+def plot(dataframe,  a_value, b_value):
+    plt.subplot(1,2,1)
+    sns.scatterplot(data = dataframe, x = "Age", y = 'Survived')
+
+    x = dataframe["Age"]
+    sns.lineplot(x= x, y= logistic_function(x, a_value, b_value), color = 'green')
+
+    plt.subplot(1,2,2)
+    sns.scatterplot(data = dataframe, x = "Pclass", y = 'Survived')
+    plt.show()
 
 
-def loss(dataframe):
-    pass
+def loss(dataframe , a_value, b_value):
+    y = dataframe["Survived"]
+    y_hat = logistic_function(dataframe["Age"], a_value , b_value)
+    return bcel(y, y_hat)
 
-logistic_function(x = "Age")
-logistic_function(x = "Pclass")
+print(loss(read_data('titanic.csv'), a_value = 1, b_value = 1))
+
+plot(read_data('titanic.csv'), a_value = 1, b_value = 1)
